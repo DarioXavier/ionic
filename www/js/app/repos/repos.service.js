ba0bd35f7166e349938196a0a14666fe00bfd1c9
Dario Vargas <d_ario13@live.com>
2017-09-21 14:56:16 -0500

    my first commit

---
+++ www/js/app/repos/repos.service.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('reposMdle')
+        .factory('reposSrvc', reposSrvc);
+
+    reposSrvc.$inject = ['$http'];
+
+    function reposSrvc($http) {
+
+        return {
+            getUserRepos: getUserRepos
+        }
+        
+        ////////////////
+
+        function getUserRepos(userName) {
+            var apiUrl = 'https://api.github.com/users/'+userName+'/repos';
+            
+            console.log('apiUrl', apiUrl);
+
+            return $http.get(apiUrl)
+            .then(function(res){
+                return res.data;
+            })
+            .catch(function(err){
+                console.log('error');
+            });
+        }
+    }
+})();
